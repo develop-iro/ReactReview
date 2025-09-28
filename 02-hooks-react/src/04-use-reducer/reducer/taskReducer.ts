e77054767bd9186d261edfd83f0d25db1cb089e9
IvanC <iro.developer@hotmail.com>
2025-09-28 15:20:27 +0200

    feat: add ScrambleWords game and TasksApp with useReducer
    - Implemented ScrambleWords component with game logic using useReducer.
    - Created reducer for ScrambleWords with actions for guessing, skipping, and starting a new game.
    - Added TasksApp component for managing tasks with add, toggle, and delete functionalities.
    - Introduced UI components: Button, Card, Checkbox, and Input for consistent styling.
    - Integrated local storage for task persistence.
    - Updated global styles and utility functions for better class management.
    - Configured TypeScript paths for cleaner imports.

---
+++ 02-hooks-react/src/04-use-reducer/reducer/taskReducer.ts
@@ -0,0 +1,105 @@
+import * as z from "zod/v4";
+
+// It must return a new state
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TaskState {
+  todos: Todo[];
+  length: number;
+  completed: number;
+  pending: number;
+}
+
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string().min(2).max(100),
+  completed: z.boolean(),
+});
+
+const TaskStateSchema = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
+export const getTaskInitialState = (): TaskState => {
+  const localStorageState = localStorage.getItem("tasks-state");
+  const defaultInitialState = {
+    todos: [],
+    length: 0,
+    completed: 0,
+    pending: 0,
+  };
+
+  if (!localStorageState) {
+    return defaultInitialState;
+  }
+
+  const result = TaskStateSchema.safeParse(JSON.parse(localStorageState));
+
+  if (result.error) return defaultInitialState;
+
+  return result.data;
+};
+
+export type TaskAction =
+  | { type: "ADD_TODO"; payload: string }
+  | { type: "TOGGLE_TODO"; payload: number }
+  | { type: "DELETE_TODO"; payload: number };
+
+export const taskReducer = (
+  state: TaskState,
+  action: TaskAction
+): TaskState => {
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+
+      return {
+        ...state,
+        todos: [...state.todos, newTodo],
+        length: state.todos.length + 1,
+        pending: state.pending + 1,
+      };
+    }
+    case "TOGGLE_TODO": {
+      const updatedTodos = state.todos.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+      return {
+        ...state,
+        todos: updatedTodos,
+        completed: updatedTodos.filter((todo) => todo.completed).length,
+        pending: updatedTodos.filter((todo) => !todo.completed).length,
+      };
+    }
+
+    case "DELETE_TODO": {
+      const currentTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload
+      );
+
+      return {
+        ...state,
+        todos: currentTodos,
+        length: currentTodos.length,
+        completed: currentTodos.filter((todo) => todo.completed).length,
+        pending: currentTodos.filter((todo) => !todo.completed).length,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
--- 02-hooks-react/src/main.tsx
+++ 02-hooks-react/src/main.tsx
@@ -1,14 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import {PokemonPage} from "./02-use-effect/PokemonPage.tsx";
+// import {PokemonPage} from "./02-use-effect/PokemonPage.tsx";
+// import { FocusPage } from "./03-use-ref/FocusPage.tsx";
+// import { TrafficLightWithEffectHook } from "./02-use-effect/TrafficLightWithEffectHook.tsx";
 // import {  TrafficLightWithEffect } from "./02-use-effect/TrafficLightWithEffect.tsx";
 // import { TrafficLight } from "./01-use-state/TrafficLight.tsx";
 // import { HookApp } from "./HookApp.tsx";
+// import { TasksApp } from "./04-use-reducer/UseReducer.tsx";
+import { ScrambleWords } from "./04-use-reducer/ScrambleWords";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PokemonPage />
+    {/* <HooksApp /> */}
+    {/* <PokemonPage /> */}
+    <ScrambleWords />
+    {/* <TasksApp /> */}
+    {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
+    {/* <TrafficLightWithEffectHook /> */}
+    {/* <FocusPage /> */}
   </StrictMode>
 );
